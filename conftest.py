598a846ddae900fd5ca0b1afdcc94e55a49cc3c4
Denis Asmankin <denis.asmankin@gmail.com>
2025-06-08 20:01:50 +0500

    add test up + conftest up + picture

--- conftest.py
+++ conftest.py
@@ -5,8 +5,9 @@ from selene import have, browser
 @pytest.fixture(scope = "function")
 def demo_qa():
     #browser.config.browser_name = 'firefox' # список стандартных driver_name: chrome, firefox, edge, remote, appium
-    browser.config.window_width = 1200
-    browser.config.window_height = 800
+    browser.driver.maximize_window() # Максимизация окна браузера
+    #browser.config.window_width = 1200
+    #browser.config.window_height = 800
     browser.config.base_url = 'https://demoqa.com/automation-practice-form'
     browser.open('/') # если прокинул конфиг браузера по base_url, тогда можно будет написать так '/'
     #browser.config.timeout = 10 # время ожидания в секундах (по дефолту в Selene 4 сек)
--- test_registration_form_submit.py
+++ test_registration_form_submit.py
@@ -1,22 +1,32 @@
+import os.path
+
 from selene import browser, be, have
-
-
 
 def test_add_data_registration_and_submit(demo_qa):
     browser.element('.practice-form-wrapper').should(be.visible and have.text('Practice Form'))
-    browser.element('#firstName').should(have.attribute('placeholder').value('First Name')).type('Petr')
-    browser.element('#lastName').should(have.attribute('placeholder').value('Last Name')).type('Petrov')
-    browser.element('#userEmail').should(have.attribute('placeholder').value('name@example.com')).type('NAME@gmail.com')
+    browser.element('#firstName').should(have.attribute('placeholder').value('First Name')).type('UserFirstName')
+    browser.element('#lastName').should(have.attribute('placeholder').value('Last Name')).type('UserLastName')
+    browser.element('#userEmail').should(have.attribute('placeholder').value('name@example.com')).type('test.email@gmail.com')
     browser.element('label[for="gender-radio-1"]').click()
-    browser.element('#userNumber').should(have.attribute('placeholder').value('Mobile Number')).type('0123456789')
-    browser.element('#dateOfBirthInput').should(have.attribute('placeholder')).click()
+    browser.element('#userNumber').should(have.attribute('placeholder').value('Mobile Number')).type('9992220000')
+    browser.element('#dateOfBirthInput').should(be.visible).click()
     browser.element('.react-datepicker__month-select').should(be.visible).click()
-    browser.element('[value="4"]').click()
+    browser.element('option[value="4"]').click()
     browser.element('.react-datepicker__year-select').should(be.visible).click()
-    browser.element('[value="2000"]').click()
+    browser.element('option[value="2000"]').click()
     browser.element('.react-datepicker__day--020').click()
     browser.element('#dateOfBirthInput').should(have.value('20 May 2000'))
-    browser.element('#subjectsInput').type('Maths').type('Physics').press_enter()
-
+    browser.element('#subjectsInput').set('Maths').press_enter()
+    browser.element('#subjectsInput').set('Physics').press_enter()
+    browser.element('label[for="hobbies-checkbox-1"]').click()
+    browser.element('#uploadPicture').send_keys(os.path.abspath('test_picture.jpg'))
+    browser.element('#currentAddress').should(have.attribute('placeholder').value('Current Address')).type('country city street house')
+    # Выбор штата и города
+    browser.element('#state').click()
+    browser.element('#react-select-3-option-3').click() #setTimeout(function(){debugger; }, 5000)
+    browser.element('#city').click()
+    browser.element('#react-select-4-option-0').click() #setTimeout(function(){debugger; }, 5000)
     breakpoint()
+
+
 
